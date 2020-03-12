ab2845b1939f664879346d9347695934d81dd2e7
Dror Rosentraub <dror.rosentraub@gmail.com>
2020-03-12 15:16:44 +0200

    Dealt with the TODO tasks

--- Glassdoor_Data_mining.py
+++ Glassdoor_Data_mining.py
@@ -4,18 +4,28 @@ import time
 import random
 import pandas as pd
 import sys
-
+import os
 """"
 In this program, we scrape Glassdoor site for job offers, using selenium.
 We create a data frame of positions with info of the role and company
 """
 PATH_OF_CHROME_DRIVER = 'chromedriver_linux64/chromedriver'
+INITIAL_LINKS = [
+    "https://www.glassdoor.com/Job/jobs.htm?sc.keyword=data%20scientist&locT=N&locId=119&locKeyword=Israel&jobType=all&fromAge=3&minSalary=0&includeNoSalaryJobs=true&radius=25&cityId=-1&minRating=0.0&industryId=-1&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0",
+    "https://www.glassdoor.com/Job/jobs.htm?sc.keyword=data%20scientist&locT=N&locId=1&locKeyword=United%20States&jobType=all&fromAge=3&minSalary=11000&includeNoSalaryJobs=true&radius=25&cityId=-1&minRating=0.0&industryId=1020&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0",
+    "https://www.glassdoor.com/Job/jobs.htm?sc.keyword=data%20scientist&locT=N&locId=1&locKeyword=United%20States&jobType=all&fromAge=3&minSalary=11000&includeNoSalaryJobs=true&radius=25&cityId=-1&minRating=0.0&industryId=1059&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0",
+    "https://www.glassdoor.com/Job/jobs.htm?sc.keyword=data%20scientist&locT=N&locId=1&locKeyword=United%20States&jobType=all&fromAge=3&minSalary=11000&includeNoSalaryJobs=true&radius=25&cityId=-1&minRating=0.0&industryId=1047&sgocId=-1&seniorityType=all&companyId=-1&employerSizes=0&applicationType=0&remoteWorkType=0",
 
+    ]
 
 def set_up_connection():
     """This function creates a connection with chrome and creates a webdriver object"""
-    driver = webdriver.Chrome(executable_path=PATH_OF_CHROME_DRIVER)  # TODO add check if path is not right
-    return driver
+
+    if not os.path.exists(PATH_OF_CHROME_DRIVER):
+        return None
+    else:
+        driver = webdriver.Chrome(executable_path=PATH_OF_CHROME_DRIVER)
+        return driver
 
 
 def gather_job_links(driver, search_link):
@@ -25,7 +35,7 @@ def gather_job_links(driver, search_link):
     :param search_link:a link to the first search page
     :return: list of links of all job posts
     """
-    driver.get(search_link)  # TODO add check if link is not right
+    driver.get(search_link)
     links = []
     while True:
         job_headers = driver.find_elements_by_class_name('jobHeader')
@@ -44,14 +54,14 @@ def gather_job_links(driver, search_link):
     return links
 
 
-def gather_data_from_links(driver, links=[]):
+def gather_data_from_links(driver, links= None):
     """
     This function goes over all links to job posts and creates a data frame out of the data pulled from each page
     :param driver: webdriver object
     :param links: list of links of job posts
     :return: data frame with data collected from all the links
     """
-    if len(links) < 1:  # TODO test this scenario
+    if links is None or len(links) < 1:
         print("No links passed to gather_data_from_links")
         sys.exit()
     glassdoor_jobs = pd.DataFrame()
@@ -64,14 +74,24 @@ def gather_data_from_links(driver, links=[]):
             pass
         try:
             glassdoor_jobs.loc[i, 'title'] = driver.find_element_by_class_name(
-                'mt-0.margBotXs.strong').text  # TODO ?put classes in constants?
+                'mt-0.margBotXs.strong').text
         except NoSuchElementException:  # if page is rejected by glassdoor, reload.
             driver.get(link)
+            time.sleep(random.randint(2, 4))
             glassdoor_jobs.loc[i, 'title'] = driver.find_element_by_class_name('mt-0.margBotXs.strong').text
+
         glassdoor_jobs.loc[i, 'company'] = driver.find_element_by_class_name('strong.ib').text
-        glassdoor_jobs.loc[i, 'location'] = driver.find_element_by_class_name('subtle.ib').text[3:]
-        glassdoor_jobs.loc[i, 'desc'] = driver.find_element_by_class_name('desc.css-58vpdc.ecgq1xb3').text.replace('\n',
+
+        try:
+            glassdoor_jobs.loc[i, 'location'] = driver.find_element_by_class_name('subtle.ib').text[3:]
+        except NoSuchElementException:
+            pass
+        try:
+            glassdoor_jobs.loc[i, 'desc'] = driver.find_element_by_class_name('desc.css-58vpdc.ecgq1xb3').text.replace('\n',
                                                                                                                    ' ')
+        except NoSuchElementException:
+            pass
+
         try:
             driver.find_element_by_xpath("//span[@class='link' and text()='Company']").click()
             time.sleep(random.randint(2, 4))
@@ -99,10 +119,17 @@ def gather_data_from_links(driver, links=[]):
 
 
 def main():
+
     driver = set_up_connection()
-    # TODO add several links and loop over them
-    links = gather_job_links(driver, "https://www.glassdoor.com/Job/israel-jobs-SRCH_IL.0,6_IN119.htm")
-    glassdoor_jobs = gather_data_from_links(driver, links)
+    if driver is None:
+        print('Add the correct path to the ChromeDriver in your machine')
+        return
+
+
+    job_links = []
+    for search_links in INITIAL_LINKS:
+        job_links.append(gather_job_links(driver, search_links))
+    glassdoor_jobs = gather_data_from_links(driver, job_links)
     glassdoor_jobs.to_excel("glassdoor_jobs_df1.xlsx")
 
 
